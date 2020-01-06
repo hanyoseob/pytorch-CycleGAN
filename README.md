ed3370f96a6a55ead49afa6e6bbc30b20d34ea2e
hanyoseob <hanyoseob@kaist.ac.kr>
2020-01-07 00:22:55 +0900

    Update display_result.py

--- README.md
+++ README.md
@@ -44,8 +44,8 @@ Image-to-image translation is a class of vision and graphics problems where the 
                      --dir_result ./results
 
 * To test using trained network, set **[scope name]** defined in the **train** phase.
-* Generated images are saved in the **images** subfolder along with **[result directory]** folder.
 * **index.html** is also generated to display the generated images.  
+* Generated images are saved in the **images** subfolder along with **[result directory]** folder.
 
 
 ## Tensorboard
--- display_result.py
+++ display_result.py
@@ -4,7 +4,7 @@ import torch
 import torchvision.utils as vutils
 import matplotlib.pyplot as plt
 
-dir_result = './result/cyclegan/monet2photo/images'
+dir_result = './results/cyclegan/monet2photo/images'
 lst_result = os.listdir(dir_result)
 
 nx = 256
@@ -14,26 +14,49 @@ nch = 3
 n = 8
 m = 6
 m_id = [0, 3, 1, 2]
-n_id = 100
 
+n_id = np.arange(len(lst_result)//m)
+np.random.shuffle(n_id)
+
+
+## From domain A to domain B
 img = torch.zeros((n*(m-4), ny, nx, nch))
 
 for i in range(m-4):
     for j in range(n):
-        p = m_id[i + 0] + m * (j + n_id)
-        # p = m_id[i + 2] + m*(j + n_id)
+        p = m_id[i + 0] + m * n_id[j]
+        # p = m_id[i + 2] + m * n_id[j]
         q = n*i + j
 
         img[q, :, :, :] = torch.from_numpy(plt.imread(os.path.join(dir_result, lst_result[p]))[:, :, :nch])
-
-        # print(lst_result[p])
 
 img = img.permute((0, 3, 1, 2))
 
 plt.figure(figsize=(n, (m-4)))
 plt.axis("off")
 # plt.title("Generated Images")
+plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
 plt.imshow(np.transpose(vutils.make_grid(img, padding=2, normalize=True), (1, 2, 0)))
 
 plt.show()
 
+## From domain B to domain A
+img = torch.zeros((n*(m-4), ny, nx, nch))
+
+for i in range(m-4):
+    for j in range(n):
+        # p = m_id[i + 0] + m * n_id[j]
+        p = m_id[i + 2] + m * n_id[j]
+        q = n*i + j
+
+        img[q, :, :, :] = torch.from_numpy(plt.imread(os.path.join(dir_result, lst_result[p]))[:, :, :nch])
+
+img = img.permute((0, 3, 1, 2))
+
+plt.figure(figsize=(n, (m-4)))
+plt.axis("off")
+# plt.title("Generated Images")
+plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+plt.imshow(np.transpose(vutils.make_grid(img, padding=2, normalize=True), (1, 2, 0)))
+
+plt.show()
